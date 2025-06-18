3c9a802659b5e57684d5835f14ad05f47824902f
jhoanchq <jhoan.chiteq@gmail.com>
2025-06-18 13:24:09 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+import Ejercicio01 from './Pages/Ejercicios/Ejercicio01.vue'
+</script>
+
+<template>
+  <Ejercicio01 />
+</template>
+
+<style scoped></style>
---
+++ src/Pages/Ejercicios/Ejercicio01.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { ref } from 'vue'
+const mensaje = ref('Sumar')
+const cambiarMensaje = () => {
+    let suma = 6 + 10
+    mensaje.value = `La suma es : ${suma}`
+}
+</script>
+
+<template>
+    <div>
+        <h1>{{ mensaje }}</h1>
+        <button @click="cambiarMensaje" class="btn btn-primary"> Cambiar Mensaje</button>
+    </div>
+</template>
+
+<style scoped></style>
